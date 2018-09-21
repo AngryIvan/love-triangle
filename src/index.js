d1577b4d78f3522099aaa49d32573475385223af
AngryIvan <leftmetalban@gmail.com>
2018-09-21 10:59:44 +0300

    updated index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,24 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+
+    var a = 0; // first Spichonee
+    var b = 0; // first Spichonee
+    var c = 0; // first Spichonee
+    var numberOfTriangles = 0;
+
+    for (var i = 0; i < preferences.length; i++) {
+        if (preferences[i] !== 0) {
+            a = preferences[i];
+            b = preferences[a - 1];
+            c = preferences[b - 1];
+            if (preferences[c - 1] == a && preferences[b - 1] == c && preferences[a - 1] == b && a !== b && b !== c && c !== a) {
+                numberOfTriangles++;
+                preferences[c - 1] = 0;
+                preferences[b - 1] = 0;
+                preferences[a - 1] = 0;
+            }
+        }
+    }
+    return numberOfTriangles;
 };
